6007a67501ae42c525f070a115b1182c5bb735fb
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-02-26 21:56:49 -0500

    debugging bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,7 +1,6 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
-
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
@@ -11,22 +10,28 @@ def selection_sort( arr ):
             if arr[smallest_index] > arr[n]:
                 smallest_index = n
 
-
         # TO-DO: swap
-
-
-
+        hold = arr[i]
+        arr[i] = arr[smallest_index]
+        arr[smallest_index] = hold
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        for n in range(0, len(arr) - 1):
+            lhs = i;
+            rhs = i + 1
+            if arr[lhs] > arr[rhs]:
+                hold = arr[lhs]
+                arr[lhs] = arr[rhs]
+                arr[rhs] = hold
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
+def count_sort(arr, maximum=-1):
     return arr
--- src/iterative_sorting/test_iterative.py
+++ src/iterative_sorting/test_iterative.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from iterative_sorting import *
+
 
 class IterativeSortingTest(unittest.TestCase):
     def test_selection_sort(self):
@@ -9,9 +9,10 @@ class IterativeSortingTest(unittest.TestCase):
         arr3 = [0, 1, 2, 3, 4, 5]
         arr4 = random.sample(range(200), 50)
 
-        self.assertEqual(selection_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+        from src.iterative_sorting.iterative_sorting import selection_sort
+        self.assertEqual(selection_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         self.assertEqual(selection_sort(arr2), [])
-        self.assertEqual(selection_sort(arr3), [0,1,2,3,4,5])
+        self.assertEqual(selection_sort(arr3), [0, 1, 2, 3, 4, 5])
         self.assertEqual(selection_sort(arr4), sorted(arr4))
 
     def test_bubble_sort(self):
@@ -20,6 +21,7 @@ class IterativeSortingTest(unittest.TestCase):
         arr3 = [0, 1, 2, 3, 4, 5]
         arr4 = random.sample(range(200), 50)
 
+        from src.iterative_sorting.iterative_sorting import bubble_sort
         self.assertEqual(bubble_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
         self.assertEqual(bubble_sort(arr2), [])
         self.assertEqual(bubble_sort(arr3), [0,1,2,3,4,5])
